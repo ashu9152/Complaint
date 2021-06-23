3686908b124418a821f308208fb2fd2fb26721ce
Ashut <omkarashu28@gmail.com>
2021-06-23 16:41:33 +0530

    Some small changes

--- app/src/main/java/com/example/complaint/MainActivity.java
+++ app/src/main/java/com/example/complaint/MainActivity.java
@@ -7,8 +7,8 @@ import android.widget.AutoCompleteTextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-String[] city, planets, language;
-AutoCompleteTextView auto, auto1, auto2;
+    String[] city, planets, language;
+    AutoCompleteTextView auto, auto1, auto2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,14 +19,18 @@ AutoCompleteTextView auto, auto1, auto2;
         auto1=findViewById(R.id.autoCompleteTextView1);
         auto2=findViewById(R.id.autoCompleteTextView2);
 
+
+        //getting resource from sting xml and showing
         city = getResources().getStringArray(R.array.City);
         ArrayAdapter<String> arrayString = new ArrayAdapter<>(this,R.layout.drop_down,city);
         auto.setAdapter(arrayString);
 
+        //getting resource from sting xml and showing using adapter
         language = getResources().getStringArray(R.array.Language);
         ArrayAdapter<String> arrayString1 = new ArrayAdapter<>(this,R.layout.drop_down,language);
         auto1.setAdapter(arrayString1);
 
+        //getting resource from sting xml and showing
         planets = getResources().getStringArray(R.array.PLanets);
         ArrayAdapter<String> arrayString2 = new ArrayAdapter<>(this,R.layout.drop_down,planets);
         auto2.setAdapter(arrayString2);
